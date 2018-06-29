65ee1669b40a19dec3bfe29cc6d732e653e0a02b
konsoul <brad.rappa@gmail.com>
2018-06-29 14:01:40 -0700

    New Changes!

--- comments.php
+++ comments.php
@@ -1,26 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
-    <meta name="viewport" content="width=
-    , initial-scale=1.0">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <meta http-equiv="X-UA-Compatible" content="ie=edge">
     <title>Document</title>
 </head>
+
 <body>
+    <?php
+    // This function below
+    echo "Hello Student";
 
-<?php
 
-$title = "Bradley Rappa Official WebSite";
+
 
 ?>
-    
-<h1><?php 
-
-echo $title;  
-
-?></h1>
-
 </body>
-</html>
 
+</html>
